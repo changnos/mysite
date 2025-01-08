1683715eec263bfa9725e1add32e549a9e6619b3
Changjin Lee <changjini32@gmail.com>
2025-01-08 14:51:52 +0900

    ...transactional #finish

--- mysite03/src/main/java/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/mysite/repository/GuestbookRepository.java
@@ -16,6 +16,10 @@ public class GuestbookRepository {
 		this.sqlSession = sqlSession;
 	}
 
+	public GuestbookVo findById(Long id) {
+		return sqlSession.selectOne("guestbook.findById", id);
+	}
+
 	public List<GuestbookVo> findAll() {
 		return sqlSession.selectList("guestbook.findAll");
 	}
